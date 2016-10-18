817cb51b5e9e06550777f55b982534dbb11b4261
valda-z <valdemar.zavadsky@microsoft.com>
2016-10-18 22:58:26 +0200

    change PORT definition

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
   });
 
-app.listen(80, function () {
+app.listen(app.get('port'), function () {
   console.log('Example app listening on port 3000!');
   });
